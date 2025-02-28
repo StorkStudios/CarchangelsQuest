71822321462fbcef9ad70b00c644f20846ea55e3
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-02-28 22:56:38 +0100

    Make car engine functional

--- Assets/Scripts/Car/CarEngine.cs
+++ Assets/Scripts/Car/CarEngine.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class CarEngine : MonoBehaviour
 {
     [SerializeField]
@@ -8,9 +9,77 @@ public class CarEngine : MonoBehaviour
     private float steeringForce;
     [SerializeField]
     private float breakForce;
+    [SerializeField]
+    private float driftFactor;
+    [SerializeField]
+    private float breakDriftFactor;
+    [SerializeField]
+    private float velocitySteerDivider;
+    [SerializeField]
+    private float handbreakLinearDamping;
+    [SerializeField]
+    private float maxForwardSpeed;
+    [SerializeField]
+    private float maxReverseSpeed;
 
-    public readonly ObservableVariable<float> steerWheel = new ObservableVariable<float>(0);
+    public readonly ObservableVariable<float> steeringWheel = new ObservableVariable<float>(0);
     public readonly ObservableVariable<float> gasPedal = new ObservableVariable<float>(0);
     public readonly ObservableVariable<bool> handbreak = new ObservableVariable<bool>(false);
 
+    private float rotationAngle;
+    private float baseDamping;
+
+    private Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rotationAngle = rigidbody.rotation;
+        baseDamping = rigidbody.linearDamping;
+    }
+
+    private void FixedUpdate()
+    {
+        if (handbreak.Value)
+        {
+            rigidbody.linearDamping = Mathf.Lerp(rigidbody.linearDamping, handbreakLinearDamping, Time.fixedDeltaTime * 3);
+        }
+        else
+        {
+            rigidbody.linearDamping = baseDamping;
+        }
+
+        ApplyEngineForce();
+
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(rigidbody.linearVelocity, transform.up);
+        Vector2 lateralVelocity = transform.right * Vector2.Dot(rigidbody.linearVelocity, transform.right);
+        Vector2 newLateralVelocity = lateralVelocity * (handbreak.Value ? breakDriftFactor : driftFactor);
+        rigidbody.linearVelocity = forwardVelocity + newLateralVelocity;
+
+        float carSteerAmount = Mathf.Clamp01(rigidbody.linearVelocity.magnitude / velocitySteerDivider);
+        rotationAngle += steeringWheel.Value * steeringForce * carSteerAmount;
+        rigidbody.MoveRotation(rotationAngle);
+    }
+
+    private void ApplyEngineForce()
+    {
+        float speed = Vector2.Dot(rigidbody.linearVelocity, transform.up);
+
+        if (speed >= maxForwardSpeed && gasPedal.Value > 0)
+        {
+            return;
+        }
+
+        if (speed <= -maxReverseSpeed && gasPedal.Value < 0)
+        {
+            return;
+        }
+
+        Vector2 engineForce = transform.up * accelerationForce * gasPedal.Value;
+        rigidbody.AddForce(engineForce);
+    }
 }
