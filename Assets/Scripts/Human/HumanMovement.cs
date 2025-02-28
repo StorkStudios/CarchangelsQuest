d147524dffb54ef0a3eca86ab6721eb33590cc0c
Pojemnik <andrzej.gauza@outlook.com>
2025-02-28 22:27:25 +0100

    Add human placeholder sprite

--- Assets/Scripts/Human/HumanMovement.cs
+++ Assets/Scripts/Human/HumanMovement.cs
@@ -21,7 +21,7 @@ public class HumanMovement : MonoBehaviour
         currentCoroutine = StartCoroutine(Walk());
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (currentCoroutine != null)
         {
