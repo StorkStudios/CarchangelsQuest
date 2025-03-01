c5e4284712d19f03a0d3ef2f5cb235670ea6ecf1
StormyData <30272275+StormyData@users.noreply.github.com>
2025-03-01 13:53:24 +0100

    add ScoreKeeper

---
+++ Assets/Scripts/GameControl/ScoreKeeper.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : Singleton<ScoreKeeper>
+{
+    private ObservableVariable<float> score = new(0);
+
+    private ObservableVariable<int> humansKilled = new(0);
+
+    private ObservableVariable<float> averageSpeed = new(0);
+
+    private LinkedList<float> speedHistory = new();
+    private float speedHistorySum = 0;
+
+    [SerializeField]
+    private float scorePerHumanKilled = 1;
+    [SerializeField]
+    private float scorePerAverageSpeed = 1;
+
+    [SerializeField]
+    private float speedWindowSeconds = 5;
+    
+    [SerializeField]
+    private PlayerKillDetector playerKillDetector;
+    [SerializeField]
+    private Rigidbody2D carBody;
+
+    void Start()
+    {
+        playerKillDetector.HumanKilled += () => {
+            humansKilled.Value += 1;
+            score.Value += scorePerHumanKilled;
+        };
+    }
+
+    void FixedUpdate()
+    {
+        int maxWindowSize = Math.Max((int)(speedWindowSeconds / Time.fixedDeltaTime), 1);
+        
+        float currentSpeed = carBody.linearVelocity.magnitude;
+
+        speedHistorySum += currentSpeed;
+        speedHistory.AddFirst(currentSpeed);
+
+        while (speedHistory.Count > maxWindowSize) {
+            float poppedSpeed = speedHistory.Last.Value;
+
+            speedHistorySum -= poppedSpeed;
+            speedHistory.RemoveLast();
+        }
+        
+        averageSpeed.Value = speedHistorySum / speedHistory.Count;
+
+        score.Value += averageSpeed.Value * scorePerAverageSpeed / Time.fixedDeltaTime;
+    }
+
+
+    public event ObservableVariable<float>.ValueChangedDelegate scoreChanged
+    {
+        add => score.ValueChanged += value;
+        remove => score.ValueChanged -= value;
+    }
+
+    public float Score => score.Value;
+
+
+    public event ObservableVariable<int>.ValueChangedDelegate humansKilledChanged
+    {
+        add => humansKilled.ValueChanged += value;
+        remove => humansKilled.ValueChanged -= value;
+    }
+
+    public float HumanKilled => humansKilled.Value;
+
+    public event ObservableVariable<float>.ValueChangedDelegate averageSpeedChanged
+    {
+        add => averageSpeed.ValueChanged += value;
+        remove => averageSpeed.ValueChanged -= value;
+    }
+
+    public float AverageSpeed => averageSpeed.Value;
+}
