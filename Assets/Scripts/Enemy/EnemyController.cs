e104caf0a8b99470900cb93c7214e5a2705ee619
Pojemnik <andrzej.gauza@outlook.com>
2025-03-01 08:18:13 +0100

    Make enemies reverse

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    private enum State
+    {
+        Chase,
+        Reverse
+    }
+
     [SerializeField]
     private Transform target;
 
@@ -11,14 +17,33 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private float steeringSensitivty;
 
-    private void Start()
-    {
-        engine.gasPedal.Value = 1;   
-    }
+    [SerializeField]
+    private Transform frontSensor;
+
+    [SerializeField]
+    [ReadOnly]
+    private State state = State.Chase;
 
     private void Update()
     {
+        if (Physics2D.OverlapBox(frontSensor.position, frontSensor.localScale, frontSensor.rotation.eulerAngles.z, LayerMask.GetMask("Wall")))
+        {
+            state = State.Reverse;
+            engine.gasPedal.Value = -1;
+        }
+        else
+        {
+            state = State.Chase;
+            engine.gasPedal.Value = 1;
+        }
         float angleTowardsTarget = Vector2.SignedAngle(transform.up, target.position - transform.position);
-        engine.steeringWheel.Value = Mathf.Clamp(angleTowardsTarget * steeringSensitivty, -1, 1);
+        engine.steeringWheel.Value = Mathf.Clamp(angleTowardsTarget * steeringSensitivty, -1, 1) * ((state == State.Reverse) ? -1 : 1);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.matrix = frontSensor.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
     }
 }
