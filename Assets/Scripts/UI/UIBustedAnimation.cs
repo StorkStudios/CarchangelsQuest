020571d7ee75e9b9e24b7bc94902cdfef1d7735a
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-03-02 01:11:42 +0100

    Add busted sign

---
+++ Assets/Scripts/UI/UIBustedAnimation.cs
@@ -0,0 +1,42 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class UIBustedAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform mask;
+    [SerializeField]
+    private float animationDuration;
+
+    private void Start()
+    {
+        FindAnyObjectByType<PlayerLoser>().CatchProgessChanged += (_, v) =>
+        {
+            if (v >= 1)
+            {
+                StartAnimation();
+            }
+        };
+    }
+
+    private void StartAnimation()
+    {
+        Vector3 position = mask.position;
+
+        Vector2 pivot = mask.pivot;
+        pivot.x = 1;
+        mask.pivot = pivot;
+
+        Vector2 anchorMin = mask.anchorMin;
+        anchorMin.x = 0;
+        mask.anchorMin = anchorMin;
+
+        Vector2 anchorMax = mask.anchorMax;
+        anchorMax.x = 0;
+        mask.anchorMax = anchorMax;
+
+        mask.position = position;
+
+        mask.DOAnchorPosX(0, animationDuration).SetEase(Ease.Linear);
+    }
+}
