5327e98c9d38d2f91e0389ac5a06301f64a753fc
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-03-02 16:03:56 +0100

    Add more music

---
+++ Assets/Scripts/CutscenePlayer.cs
@@ -0,0 +1,60 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CutscenePlayer : MonoBehaviour
+{
+    [System.Serializable]
+    private class Scene
+    {
+        public Sprite background;
+        public AudioClip voiceover;
+        [TextArea]
+        public string text;
+        public Vector2 delays;
+    }
+
+    [SerializeField]
+    private List<Scene> scenes;
+    [SerializeField]
+    private string sceneToLoad;
+    [SerializeField]
+    private Image background;
+    [SerializeField]
+    private Image courtain;
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private float backgroundFadeDuration;
+
+    private void Start()
+    {
+        StartCoroutine(CutsceneCoroutine());
+    }
+
+    private IEnumerator CutsceneCoroutine()
+    {
+        foreach (var scene in scenes)
+        {
+            background.sprite = scene.background;
+            audioSource.clip = scene.voiceover;
+
+            courtain.DOFade(0, backgroundFadeDuration / 2);
+
+            yield return new WaitForSeconds(scene.delays.x);
+
+            audioSource.Play();
+
+            yield return new WaitWhile(() => audioSource.isPlaying);
+
+            yield return new WaitForSeconds(scene.delays.y);
+
+            courtain.DOFade(1, backgroundFadeDuration / 2);
+        }
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
