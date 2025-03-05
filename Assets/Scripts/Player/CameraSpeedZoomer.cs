18fd2e73800e66b54a54382cb0fa8d305e109c3d
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-03-05 10:50:46 +0100

    Update camera zoomer script

--- Assets/Scripts/Player/CameraSpeedZoomer.cs
+++ Assets/Scripts/Player/CameraSpeedZoomer.cs
@@ -4,25 +4,52 @@ using UnityEngine;
 public class CameraSpeedZoomer : MonoBehaviour
 {
     [SerializeField]
+    private float sleepCameraSize;
+    [SerializeField]
+    private float sleepSpeed;
+    [SerializeField]
+    private float sleepTime;
+    [SerializeField]
     private RangeBoundariesFloat cameraSize;
-
     [SerializeField]
     private RangeBoundariesFloat speedRange;
+    [SerializeField]
+    private float cameraSpeed;
 
     [SerializeField]
     private Rigidbody2D playerRigidbody;
 
     private Camera camera;
 
+    private float lastAwakeTime;
+    private float cameraTarget;
+
     private void Start()
     {
-        camera = GetComponent<Camera>();   
+        camera = GetComponent<Camera>();
+        cameraTarget = sleepCameraSize;
+        lastAwakeTime = Time.time - sleepTime;
     }
 
     private void Update()
     {
-        float t = Mathf.Clamp01(speedRange.NormalizeValue(playerRigidbody.linearVelocity.magnitude));
+        float speed = playerRigidbody.linearVelocity.magnitude;
+        if (speed <= sleepSpeed)
+        {
+            float sleepDuration = Time.time - lastAwakeTime;
+            if (sleepDuration >= sleepTime)
+            {
+                cameraTarget = sleepCameraSize;
+            }
+        }
+        else
+        {
+            float t = Mathf.Clamp01(speedRange.NormalizeValue(speed));
+            cameraTarget = Mathf.Lerp(cameraSize.Min, cameraSize.Max, t);
 
-        camera.orthographicSize = Mathf.Lerp(cameraSize.Min, cameraSize.Max, t);
+            lastAwakeTime = Time.time;
+        }
+
+        camera.orthographicSize = Mathf.MoveTowards(camera.orthographicSize, cameraTarget, cameraSpeed * Time.deltaTime);
     }
 }
